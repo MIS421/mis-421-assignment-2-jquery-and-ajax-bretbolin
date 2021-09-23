19d4707ef729bc944ddab39cabf289458441b9f4
brbolin <brbolin@umail.iu.edu>
2021-09-23 13:07:55 -0500

    Working Project

--- script.js
+++ script.js
@@ -10,9 +10,9 @@ function apiSearch() {
   };
 
   $.ajax({
-      url: 'my-api-url' + $.param(params),
+      url: 'https://api.bing.microsoft.com/v7.0/search?' + $.param(params),
       beforeSend: function (xhrObj) {
-        xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", "my-api-key");
+          xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", "91805d8bb0d843c397992dcaf52f1fa9");
       },
       type: "GET",
     })
@@ -29,3 +29,33 @@ function apiSearch() {
       alert("error");
     });
 }
+
+function show_time() {
+    var my_time = new Date();
+    var str = my_time.getHours() + ':' + my_time.getMinutes();
+    document.getElementById('time').innerHTML = str;
+}
+
+
+function changeBackground() {
+    document.body.style.backgroundImage = "url('https://images.unsplash.com/photo-1631852393772-99c81a40edf5?ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDUzfDZzTVZqVExTa2VRfHxlbnwwfHx8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60')";
+}
+
+
+$(function show_time() {
+    $("#time").dialog({
+        autoOpen: false,
+        show: {
+            effect: "blind",
+            duration: 1000
+        },
+        hide: {
+            effect: "blind",
+            duration: 1000
+        }
+    });
+
+    $("#opener").on("click", function () {
+        $("#time").dialog("open");
+    });
+});
